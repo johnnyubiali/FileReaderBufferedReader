91ecc3c04edc0b2d465fdd65d6566f5d0c894365
johnnyubiali <johnnyubiali@hotmail.com>
2022-07-11 16:47:41 -0300

    Program Class Modified - Try with Resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,12 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		String path = "F:\\Cursos e Apostilas\\Curso Java\\workspace-eclipse\\FileReaderBufferedReader\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
 			
 			String line = br.readLine();
 			
@@ -25,19 +21,6 @@ public class Program {
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			try {
-				if(br != null) {
-				   br.close();
-				}
-				if(fr != null) {
-				fr.close();
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 		
 	}
