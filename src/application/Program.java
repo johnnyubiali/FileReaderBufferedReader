a951ff6bbf6aa4bb9e7cfe1b4e109c6ee62f5b1c
johnnyubiali <johnnyubiali@hotmail.com>
2022-07-11 16:33:26 -0300

    File List Created

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,8 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String path = "F:\\Cursos e Apostilas\\Curso Java\\workspace-eclipse\\FileReaderBufferedReader\\in.txt";
 	}
 
 }
